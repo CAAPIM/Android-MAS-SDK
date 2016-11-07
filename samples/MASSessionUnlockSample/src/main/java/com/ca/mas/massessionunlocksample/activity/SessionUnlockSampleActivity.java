f630ad9bd0d93a7c5107d67e4dbd47a98f90acb1
Kung, Fergus H <Fergus.Kung@ca.com>
2016-11-07 15:25:16 -0800

    [US240423] Fingerprint sample app work

--- samples/MASSessionUnlockSample/src/main/java/com/ca/mas/massessionunlocksample/activity/SessionUnlockSampleActivity.java
+++ samples/MASSessionUnlockSample/src/main/java/com/ca/mas/massessionunlocksample/activity/SessionUnlockSampleActivity.java
@@ -9,47 +9,101 @@ package com.ca.mas.massessionunlocksample.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.widget.CheckBox;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.ca.mas.foundation.MAS;
 import com.ca.mas.foundation.MASCallback;
+import com.ca.mas.foundation.MASRequest;
+import com.ca.mas.foundation.MASResponse;
 import com.ca.mas.foundation.MASUser;
 import com.ca.mas.massessionunlocksample.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SessionUnlockSampleActivity extends AppCompatActivity {
     private final String TAG = SessionUnlockSampleActivity.class.getSimpleName();
     private Context mContext;
+    private RelativeLayout mContainer;
+    private Button mLoginButton;
+    private Button mLogoutButton;
+    private Switch mLockSwitch;
+    private TextView mProtectedContent;
     private FloatingActionButton mLockButton;
-    private CheckBox mLockCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_session_unlock);
+
+        // Prevents screenshotting of content in Recents
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
+                WindowManager.LayoutParams.FLAG_SECURE);
+
+        setContentView(R.layout.activity_session_unlock_sample);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
         mContext = this;
+        mContainer = (RelativeLayout) findViewById(R.id.container);
         mLockButton = (FloatingActionButton) findViewById(R.id.fab);
-        mLockCheckBox = (CheckBox) findViewById(R.id.checkbox_lock);
+        mLoginButton = (Button) findViewById(R.id.login_button);
+        mLogoutButton = (Button) findViewById(R.id.logout_button);
+        mLockSwitch = (Switch) findViewById(R.id.checkbox_lock);
+        mProtectedContent = (TextView) findViewById(R.id.data_text_view);
 
         MAS.start(this, true);
-        MASUser.login("username", "password", getUserCallback());
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MASUser.login("admin", "7layer", getLoginCallback());
+            }
+        });
+
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MASUser.getCurrentUser().logout(getLogoutCallback());
+            }
+        });
     }
 
-    private MASCallback<MASUser> getUserCallback() {
+    private MASCallback<MASUser> getLoginCallback() {
         return new MASCallback<MASUser>() {
             @Override
             public void onSuccess(MASUser user) {
                 mLockButton.setOnClickListener(getLockButtonListener());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mLockButton.setVisibility(View.VISIBLE);
+                        mLockSwitch.setVisibility(View.VISIBLE);
+                        mLoginButton.setEnabled(false);
+                        mLogoutButton.setEnabled(true);
+                    }
+                });
+
+                invokeApi();
             }
 
             @Override
@@ -57,6 +111,71 @@ public class SessionUnlockSampleActivity extends AppCompatActivity {
                 Log.e(TAG + " getUserCallback()", e.toString());
             }
         };
+    }
+
+    private MASCallback<Void> getLogoutCallback() {
+        return new MASCallback<Void>() {
+            @Override
+            public Handler getHandler() {
+                return new Handler(Looper.getMainLooper());
+            }
+
+            @Override
+            public void onSuccess(Void result) {
+                mLockButton.setVisibility(View.GONE);
+                mLockSwitch.setVisibility(View.GONE);
+                mLogoutButton.setEnabled(false);
+                mLoginButton.setEnabled(true);
+                mProtectedContent.setText(R.string.protected_info);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Snackbar.make(mContainer, e.toString(), Snackbar.LENGTH_LONG).show();
+            }
+        };
+    }
+
+    private void invokeApi() {
+        String path = "/protected/resource/products";
+        Uri.Builder uriBuilder = new Uri.Builder().encodedPath(path);
+        uriBuilder.appendQueryParameter("operation", "listProducts");
+        uriBuilder.appendQueryParameter("pName2", "pValue2");
+
+        MASRequest.MASRequestBuilder requestBuilder = new MASRequest.MASRequestBuilder(uriBuilder.build());
+        requestBuilder.header("hName1", "hValue1");
+        requestBuilder.header("hName2", "hValue2");
+        MASRequest request = requestBuilder.get().build();
+
+        MAS.invoke(request, new MASCallback<MASResponse<JSONObject>>() {
+            @Override
+            public Handler getHandler() {
+                return new Handler(Looper.getMainLooper());
+            }
+
+            @Override
+            public void onSuccess(MASResponse<JSONObject> result) {
+                try {
+                    List<String> objects = parseProductListJson(result.getBody().getContent());
+                    String objectString = "";
+                    int size = objects.size();
+                    for (int i = 0; i < size; i++) {
+                        objectString += objects.get(i);
+                        if (i != size - 1) {
+                            objectString += "\n";
+                        }
+                    }
+
+                    mProtectedContent.setText(objectString);
+                } catch (JSONException e) {
+
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+            }
+        });
     }
 
     private View.OnClickListener getLockButtonListener() {
@@ -72,7 +191,7 @@ public class SessionUnlockSampleActivity extends AppCompatActivity {
         return new MASCallback<Void>() {
             @Override
             public void onSuccess(Void result) {
-                if (mLockCheckBox.isChecked()) {
+                if (mLockSwitch.isChecked()) {
                     launchLockActivity();
                 }
             }
@@ -85,16 +204,32 @@ public class SessionUnlockSampleActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        MASUser currentUser = MASUser.getCurrentUser();
-        if (currentUser != null && currentUser.isSessionLocked()) {
+    protected void onPause() {
+        if (mLockSwitch.getVisibility() == View.VISIBLE && mLockSwitch.isChecked()) {
             launchLockActivity();
         }
+        super.onPause();
     }
 
     private void launchLockActivity() {
         Intent i = new Intent("MASUI.intent.action.SessionUnlock");
         startActivity(i);
+    }
+
+    private static List<String> parseProductListJson(JSONObject json) throws JSONException {
+        try {
+            List<String> objects = new ArrayList<>();
+            JSONArray items = json.getJSONArray("products");
+            for (int i = 0; i < items.length(); i++) {
+                JSONObject item = (JSONObject) items.get(i);
+                Integer id = (Integer) item.get("id");
+                String name = (String) item.get("name");
+                String price = (String) item.get("price");
+                objects.add(id + ": " + name + ", $" + price);
+            }
+            return objects;
+        } catch (ClassCastException e) {
+            throw (JSONException) new JSONException("Response JSON was not in the expected format").initCause(e);
+        }
     }
 }

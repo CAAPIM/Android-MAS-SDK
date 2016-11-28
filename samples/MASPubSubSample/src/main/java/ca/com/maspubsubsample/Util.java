007571ad320740d184de900a6a7c24bfd5180d11
Saad <saad.kalim@ca.com>
2016-11-28 13:53:42 -0800

    [US240502] - Remove unused imports.

--- samples/MASPubSubSample/src/main/java/ca/com/maspubsubsample/Util.java
+++ samples/MASPubSubSample/src/main/java/ca/com/maspubsubsample/Util.java
@@ -1,7 +1,6 @@
 package ca.com.maspubsubsample;
 
 import android.app.Activity;
-import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.view.View;

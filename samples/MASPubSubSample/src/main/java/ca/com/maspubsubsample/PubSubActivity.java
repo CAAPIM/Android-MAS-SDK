f2cc13df70c5aba9adea0a6ec9cc7d3bbd2cbff1
Saad <saad.kalim@ca.com>
2016-11-28 17:09:01 -0800

    [US240502] - Added pub sub sample app

---
+++ samples/MASPubSubSample/src/main/java/ca/com/maspubsubsample/PubSubActivity.java
@@ -0,0 +1,136 @@
+package ca.com.maspubsubsample;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.ca.mas.connecta.client.MASConnectaManager;
+import com.ca.mas.connecta.util.ConnectaConsts;
+import com.ca.mas.foundation.MASCallback;
+import com.ca.mas.foundation.MASException;
+import com.ca.mas.messaging.MASMessage;
+
+public class PubSubActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = PubSubActivity.class.getSimpleName();
+
+    private MessagesFragment messagesFragment;
+    private boolean publicBroker;
+    View mainView;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pub_sub);
+        Intent i = getIntent();
+        if (i != null) {
+            publicBroker = i.getBooleanExtra(MainActivity.INTENT_EXTRA_PUBLIC_BROKER, false);
+            String host = i.getStringExtra(MainActivity.INTENT_EXTRA_HOST);
+            TextView textViewHost = (TextView) findViewById(R.id.activity_pub_sub_text_view_host);
+            textViewHost.setText(host);
+        }
+        mainView = findViewById(R.id.activity_pub_sub_main_view);
+        progressBar = (ProgressBar) findViewById(R.id.activity_pub_sub_progress_bar);
+        messagesFragment = (MessagesFragment) getSupportFragmentManager().findFragmentById(R.id.activity_pub_sub_fragment_messages);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ConnectaConsts.MAS_CONNECTA_BROADCAST_MESSAGE_ARRIVED);
+        LocalBroadcastManager.getInstance(this).registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (!intent.getAction().equals(ConnectaConsts.MAS_CONNECTA_BROADCAST_MESSAGE_ARRIVED)) {
+                    return;
+                }
+
+                try {
+                    MASMessage message = MASMessage.newInstance(intent);
+                    messagesFragment.onMessageReceived(message);
+                } catch (MASException e) {
+                    Log.d(TAG, e.getMessage());
+                }
+            }
+        }, intentFilter);
+
+        findViewById(R.id.activity_pub_sub_fab).setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_pub_sub, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_disconnect:
+                showProgress();
+                MASConnectaManager.getInstance().disconnect(new MASCallback<Void>() {
+                    @Override
+                    public void onSuccess(Void result) {
+                        Intent i = new Intent(PubSubActivity.this, MainActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        hideProgress();
+                        Util.showSnackbar("Failed to disconnect");
+                    }
+                });
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showProgress() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mainView.setVisibility(View.GONE);
+                progressBar.setVisibility(View.VISIBLE);
+
+            }
+        });
+    }
+
+    private void hideProgress() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                progressBar.setVisibility(View.GONE);
+                mainView.setVisibility(View.VISIBLE);
+
+            }
+        });
+    }
+
+    public boolean isPublicBroker() {
+        return publicBroker;
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.activity_pub_sub_fab:
+                PublishDialogFragment publishDialogFragment = new PublishDialogFragment();
+                publishDialogFragment.show(getSupportFragmentManager(), null);
+                break;
+        }
+    }
+}
---
+++ samples/MASPubSubSample/src/main/java/ca/com/maspubsubsample/Util.java
@@ -0,0 +1,31 @@
+package ca.com.maspubsubsample;
+
+import android.app.Activity;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+public class Util {
+
+    public static void showSnackbar(String message) {
+        showSnackbar(message, null);
+    }
+
+    public static void showSnackbar(String message, CoordinatorLayout coordinatorLayout) {
+        Snackbar snackbar = Snackbar
+                .make(coordinatorLayout, message, Snackbar.LENGTH_SHORT);
+        snackbar.show();
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+}

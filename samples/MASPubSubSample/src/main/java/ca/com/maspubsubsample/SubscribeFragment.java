8fcfd82205becfcdb8222d06a7f7d2ef6c4d6468
Andy <witan03@ca.com>
2016-12-07 11:53:04 -0800

    [US273439] -Update copyright for pub sub sample App

--- samples/MASPubSubSample/src/main/java/ca/com/maspubsubsample/SubscribeFragment.java
+++ samples/MASPubSubSample/src/main/java/ca/com/maspubsubsample/SubscribeFragment.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2016 CA. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ *
+ */
+
 package ca.com.maspubsubsample;
 
 

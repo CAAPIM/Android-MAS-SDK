322c88dbb35da66d770e49671af60c383aed8b5f
Kung, Fergus H <Fergus.Kung@ca.com>
2016-12-13 11:52:44 -0800

    [US273008] Updating CustomTabs workflow with successful auth code retrieval from backend, requires Andy's custom build of AppAuth

--- MASUI/src/main/java/com/ca/mas/ui/CustomTabs.java
+++ MASUI/src/main/java/com/ca/mas/ui/CustomTabs.java
@@ -1,77 +1,58 @@
 package com.ca.mas.ui;
 
-
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.annotation.NonNull;
 import android.util.Log;
 
-import com.ca.mas.core.io.ssl.MAGSocketFactory;
 import com.ca.mas.foundation.MAS;
 import com.ca.mas.foundation.MASCallback;
 import com.ca.mas.foundation.auth.MASAuthenticationProvider;
 import com.ca.mas.foundation.auth.MASSocialLogin;
 
-import net.openid.appauth.AppAuthConfiguration;
 import net.openid.appauth.AuthorizationRequest;
 import net.openid.appauth.AuthorizationService;
 import net.openid.appauth.AuthorizationServiceConfiguration;
-import net.openid.appauth.connectivity.ConnectionBuilder;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLDecoder;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class CustomTabs {
-
-    public static void socialLogin(final Context context, MASAuthenticationProvider provider){
+    public static void socialLogin(final Context context, MASAuthenticationProvider provider) {
         MASSocialLogin.getAuthConfiguration(context, provider, new MASCallback<Uri>() {
             @Override
             public void onSuccess(Uri uri) {
+                String clientId = uri.getQueryParameter("client_id");
+                String redirectUri = uri.getQueryParameter("redirect_uri");
+                String scope = uri.getQueryParameter("scope");
+                String state = uri.getQueryParameter("state");
+                String responseType = uri.getQueryParameter("response_type");
+
                 try {
                     URL url = new URL(uri.toString());
-                    Map<String, List<String>> query = splitQuery(url);
-
                     String configuration = url.getProtocol() + "://" + url.getAuthority() + url.getPath();
-                    String clientId = query.get("client_id").get(0);
-                    String redirectUri = query.get("redirect_uri").get(0);
-                    String state = query.get("state").get(0);
-                    String responseType = query.get("response_type").get(0);
-                    String[] scope = query.get("scope").get(0).split("\\s+");
-
-                    AuthorizationServiceConfiguration config =
-                            new AuthorizationServiceConfiguration(Uri.parse(configuration),
-                                    Uri.parse("https://login.salesforce.com/services/oauth2/token"), null);
+                    Uri authEndpoint = Uri.parse(configuration);
+                    Uri tokenEndpoint = Uri.parse("");
+                    AuthorizationServiceConfiguration config = new AuthorizationServiceConfiguration(
+                            authEndpoint, tokenEndpoint, null);
 
                     AuthorizationRequest req = new AuthorizationRequest.Builder(
-                            config,
-                            clientId,
-                            responseType,
-                            Uri.parse(redirectUri))
+                            config, clientId, responseType, Uri.parse(redirectUri))
                             .setState(state)
                             .setScopes(scope)
                             .setCodeVerifier(null)
                             .build();
 
-                    Intent postAuthIntent = new Intent(MAS.getContext(), Empty.class);
-                    Intent authCancelledIntent = new Intent(MAS.getContext(), Empty.class);
+                    Context context = MAS.getContext();
+                    Intent postAuthIntent = new Intent(context, MASOAuthRedirectActivity.class);
+                    Intent authCanceledIntent = new Intent(context, Empty.class);
 
                     AuthorizationService service = new AuthorizationService(MAS.getContext());
-                    service.performAuthorizationRequest(req, PendingIntent.getActivity(MAS.getContext(),
-                            req.hashCode(), postAuthIntent, 0),
-                            PendingIntent.getActivity(MAS.getContext(), req.hashCode(), authCancelledIntent, 0));
-                } catch (UnsupportedEncodingException | MalformedURLException e) {
+                    service.performAuthorizationRequest(req,
+                            PendingIntent.getActivity(context, req.hashCode(), postAuthIntent, 0),
+                            PendingIntent.getActivity(context, req.hashCode(), authCanceledIntent, 0));
+                } catch (MalformedURLException e) {
                     Log.d("", e.getMessage());
                 }
             }
@@ -81,20 +62,5 @@ public class CustomTabs {
                 Log.d("CustomTabs", e.getMessage());
             }
         });
-    }
-
-    private static Map<String, List<String>> splitQuery(URL url) throws UnsupportedEncodingException {
-        final Map<String, List<String>> query_pairs = new LinkedHashMap<String, List<String>>();
-        final String[] pairs = url.getQuery().split("&");
-        for (String pair : pairs) {
-            final int idx = pair.indexOf("=");
-            final String key = idx > 0 ? URLDecoder.decode(pair.substring(0, idx), "UTF-8") : pair;
-            if (!query_pairs.containsKey(key)) {
-                query_pairs.put(key, new LinkedList<String>());
-            }
-            final String value = idx > 0 && pair.length() > idx + 1 ? URLDecoder.decode(pair.substring(idx + 1), "UTF-8") : null;
-            query_pairs.get(key).add(value);
-        }
-        return query_pairs;
     }
 }
---
+++ masui/src/main/java/com/ca/mas/ui/MASOAuthRedirectActivity.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2016 CA. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ *
+ */
+package com.ca.mas.ui;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MASOAuthRedirectActivity extends AppCompatActivity {
+    private static final String USED_INTENT = "USED_INTENT";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+        Uri redirectUri = getIntent().getData();
+        String authorizationCode = redirectUri.getQueryParameter("code");
+        if (authorizationCode != null) {
+            //MASUser.getCurrentUser().login(authorizationCode);
+        }
+    }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        checkIntent(getIntent());
+//    }
+//
+//    @Override
+//    protected void onNewIntent(Intent intent) {
+//        checkIntent(intent);
+//    }
+//
+//    private void checkIntent(@Nullable Intent intent) {
+//        if (intent != null) {
+//            String action = intent.getAction();
+//            switch (action) {
+//                case "com.masui.oauth.HANDLE_AUTHORIZATION_RESPONSE":
+//                    if (!intent.hasExtra(USED_INTENT)) {
+//                        handleAuthorizationResponse(intent);
+//                        intent.putExtra(USED_INTENT, true);
+//                    }
+//                    break;
+//                default:
+//                    // do nothing
+//            }
+//        }
+//    }
+//
+//    /**
+//     * Exchanges the code, for the {@link TokenResponse}.
+//     *
+//     * @param intent represents the {@link Intent} from the Custom Tabs or the System Browser.
+//     */
+//    private void handleAuthorizationResponse(@NonNull Intent intent) {
+//        AuthorizationResponse response = AuthorizationResponse.fromIntent(intent);
+//        AuthorizationException error = AuthorizationException.fromIntent(intent);
+//        final AuthState authState = new AuthState(response, error);
+//
+//        if (response != null) {
+//            Log.i(TAG, String.format("Handled Authorization Response %s ", authState.toString()));
+//            AuthorizationService service = new AuthorizationService(this);
+//            service.performTokenRequest(response.createTokenExchangeRequest(), new AuthorizationService.TokenResponseCallback() {
+//                @Override
+//                public void onTokenRequestCompleted(@Nullable TokenResponse tokenResponse, @Nullable AuthorizationException exception) {
+//                    if (exception != null) {
+//                        Log.w(TAG, "Token Exchange failed", exception);
+//                    } else {
+//                        if (tokenResponse != null) {
+//                            authState.update(tokenResponse, exception);
+//                            persistAuthState(authState);
+//                            Log.i(TAG, String.format("Token Response [ Access Token: %s, ID Token: %s ]", tokenResponse.accessToken, tokenResponse.idToken));
+//                        }
+//                    }
+//                }
+//            });
+//        }
+//    }
+}

f95c35b76127536a840c1bf4145ca7a3b6457383
Andy <witan03@ca.com>
2017-01-12 12:38:24 -0800

    [US273008] - Update javadoc for MASAuthorizationResponse

--- mas/src/main/java/com/ca/mas/foundation/MASAuthorizationResponse.java
+++ mas/src/main/java/com/ca/mas/foundation/MASAuthorizationResponse.java
@@ -11,30 +11,38 @@ package com.ca.mas.foundation;
 import android.net.Uri;
 
 /**
- * Authorization Response to a authorization request
+ * Response to an authorization request
  * {@link com.ca.mas.foundation.auth.MASAuthenticationProviders#getAuthenticationProviders(MASCallback)}
  *
  * @see <a href="https://tools.ietf.org/html/rfc6749#section-4.1.2">
  */
 public class MASAuthorizationResponse {
 
-   private String authorizationCode;
-   private String state;
+    //The returned authorization code parameter from the redirect uri
+    private String authorizationCode;
+    //The returned state parameter from the redirect uri
+    private String state;
 
-   public MASAuthorizationResponse(String authorizationCode, String state) {
-      this.authorizationCode = authorizationCode;
-      this.state = state;
-   }
 
-   public static MASAuthorizationResponse fromUri(Uri uri) {
-      return new MASAuthorizationResponse(uri.getQueryParameter("code"), uri.getQueryParameter("state"));
-   }
+    public MASAuthorizationResponse(String authorizationCode, String state) {
+        this.authorizationCode = authorizationCode;
+        this.state = state;
+    }
 
-   public String getAuthorizationCode() {
-      return authorizationCode;
-   }
+    /**
+     * Extracts an authorization response from redirect uri.
+     * The {@link MASAuthorizationResponse} can be used for {@link MASUser#login(MASAuthorizationResponse, MASCallback)}
+     * to authenticate the user.
+     */
+    public static MASAuthorizationResponse fromUri(Uri uri) {
+        return new MASAuthorizationResponse(uri.getQueryParameter("code"), uri.getQueryParameter("state"));
+    }
 
-   public String getState() {
-      return state;
-   }
+    public String getAuthorizationCode() {
+        return authorizationCode;
+    }
+
+    public String getState() {
+        return state;
+    }
 }

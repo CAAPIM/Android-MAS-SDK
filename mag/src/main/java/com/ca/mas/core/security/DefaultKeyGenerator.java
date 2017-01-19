4f2caee04ca940207ab73df8255615290177f393
schsa17@ca.com <schsa17@ca.com>
2017-01-19 09:31:37 -0500

    add creation of symmetric key for Android M+

--- mag/src/main/java/com/ca/mas/core/security/DefaultKeyGenerator.java
+++ mag/src/main/java/com/ca/mas/core/security/DefaultKeyGenerator.java
@@ -50,4 +50,25 @@ class DefaultKeyGenerator implements KeyGenerator {
         return kg.generateKey();
 
     }
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public SecretKey createSymmetricKeyAndroidM(String SYMMETRIC_KEY_ALIAS)
+               throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(
+                    KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+        keyGenerator.init(
+                    new KeyGenParameterSpec.Builder(SYMMETRIC_KEY_ALIAS,
+                            KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                            .setKeySize(256)
+                            .setBlockModes(BLOCK_MODE_CBC, BLOCK_MODE_CTR, BLOCK_MODE_GCM)
+                            .setDigests(DIGEST_NONE, DIGEST_MD5, DIGEST_SHA1, DIGEST_SHA256, DIGEST_SHA384, DIGEST_SHA512)
+                            .setEncryptionPaddings(ENCRYPTION_PADDING_NONE, ENCRYPTION_PADDING_PKCS7, ENCRYPTION_PADDING_RSA_OAEP, ENCRYPTION_PADDING_RSA_PKCS1)
+                            .setRandomizedEncryptionRequired(false)
+                            .setUserAuthenticationRequired(false)
+                            .build());
+        SecretKey key = keyGenerator.generateKey();
+        return key;
+    }
+
 }

f7ec8955bbbd8762c5e4dd27d7494182ef9cebc1
Andy <witan03@ca.com>
2017-04-17 12:27:53 -0700

    Rename KeyUtils to KeyUtilsAsymmetric.

--- mag/magtest/src/androidTest/java/com/ca/mas/core/test/SignJWTTest.java
+++ mag/magtest/src/androidTest/java/com/ca/mas/core/test/SignJWTTest.java
@@ -10,12 +10,8 @@ package com.ca.mas.core.test;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.ca.mas.core.conf.ConfigurationManager;
-import com.ca.mas.core.conf.Server;
 import com.ca.mas.core.http.ContentType;
-import com.ca.mas.core.store.StorageProvider;
-import com.ca.mas.core.store.TokenManager;
-import com.ca.mas.core.util.KeyUtils;
+import com.ca.mas.core.util.KeyUtilsAsymmetric;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.JWSObject;
@@ -26,18 +22,13 @@ import com.nimbusds.jose.crypto.RSASSAVerifier;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.jwt.util.DateUtils;
 
-import net.minidev.json.reader.JsonWriterI;
-
-import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.interfaces.RSAPublicKey;
-import java.util.Date;
 import java.util.UUID;
 
 import static junit.framework.Assert.assertEquals;
@@ -48,50 +39,20 @@ public class SignJWTTest {
     @Test
     public void testSignAndVerify() throws Exception {
         //Client
-        PrivateKey privateKey = KeyUtils.generateRsaPrivateKey(InstrumentationRegistry.getInstrumentation().getTargetContext(), 2048, "TEST", "dn=test", false, false, -1, false);
+        PrivateKey privateKey = KeyUtilsAsymmetric.generateRsaPrivateKey(InstrumentationRegistry.getInstrumentation().getTargetContext(), 2048, "TEST", "dn=test", false, false, -1, false);
         JWSObject jwsObject = new JWSObject(new JWSHeader(JWSAlgorithm.RS256),
                 new Payload("Hello, world!"));
         jwsObject.sign(new RSASSASigner(privateKey));
         String compactJws = jwsObject.serialize();
 
         //Server
-        JWSVerifier verifier = new RSASSAVerifier((RSAPublicKey) KeyUtils.getRsaPublicKey("TEST"));
+        JWSVerifier verifier = new RSASSAVerifier((RSAPublicKey) KeyUtilsAsymmetric.getRsaPublicKey("TEST"));
 
         assertTrue(jwsObject.verify(verifier));
 
         assertEquals("Hello, world!", jwsObject.getPayload().toString());
 
-        KeyUtils.deletePrivateKey("TEST");
-    }
-
-    @Test
-    public void name() throws Exception {
-
-        JWTClaimsSet.Builder claimBuilder = new JWTClaimsSet.Builder();
-        claimBuilder.claim("content", true);
-        claimBuilder.claim("content-type", ContentType.APPLICATION_JSON.getMimeType());
-
-        String s = claimBuilder.build().toJSONObject().toJSONString();
-        System.out.print(s);
-
-    }
-
-    private static class Test2 {
-        private String name;
-        private String value;
-
-        public Test2(String name, String value) {
-            this.name = name;
-            this.value = value;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getValue() {
-            return value;
-        }
+        KeyUtilsAsymmetric.deletePrivateKey("TEST");
     }
 
     @Test
@@ -99,8 +60,8 @@ public class SignJWTTest {
 
         //===================== Enroll ======================================
         //The client enroll the public key with JWK
-        PrivateKey privateKey = KeyUtils.generateRsaPrivateKey(InstrumentationRegistry.getInstrumentation().getTargetContext(), 2048, "TEST", "dn=test", false, false, -1, false);
-        PublicKey publicKey = KeyUtils.getRsaPublicKey("TEST");
+        PrivateKey privateKey = KeyUtilsAsymmetric.generateRsaPrivateKey(InstrumentationRegistry.getInstrumentation().getTargetContext(), 2048, "TEST", "dn=test", false, false, -1, false);
+        PublicKey publicKey = KeyUtilsAsymmetric.getRsaPublicKey("TEST");
 
         JWK jwk = new RSAKey.Builder((RSAPublicKey) publicKey)
                 .keyID(UUID.randomUUID().toString()) // Give the key some ID (optional)
@@ -126,6 +87,6 @@ public class SignJWTTest {
         assertEquals("Hello, world!", jwsObject.getPayload().toString());
 
         //Clean up for the test
-        KeyUtils.deletePrivateKey("TEST");
+        KeyUtilsAsymmetric.deletePrivateKey("TEST");
     }
 }

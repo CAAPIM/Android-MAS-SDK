53ccddb83bd0774ed1ad9f91a791529681b2c539
Kung, Fergus <Fergus.Kung@ca.com>
2017-04-25 12:06:27 -0700

    [US332907] Session lock/unlock test cases

---
+++ mas/src/androidTest/java/com/ca/mas/foundation/MASSessionLockTest.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2016 CA. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ *
+ */
+package com.ca.mas.foundation;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+
+import com.ca.mas.MASCallbackFuture;
+import com.ca.mas.MASLoginTestBase;
+import com.ca.mas.MASSessionUnlockCallbackFuture;
+import com.ca.mas.core.security.SecureLockException;
+import com.ca.mas.core.store.StorageProvider;
+import com.ca.mas.core.store.TokenManager;
+import com.ca.mas.core.store.TokenStoreException;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
+
+public class MASSessionLockTest extends MASLoginTestBase {
+
+    @Test
+    @TargetApi(Build.VERSION_CODES.M)
+    public void testLockSession() throws Exception {
+        MASUser currentUser = MASUser.getCurrentUser();
+        MASCallbackFuture<Void> callbackFuture = new MASCallbackFuture<>();
+        currentUser.lockSession(callbackFuture);
+        try {
+            callbackFuture.get();
+        } catch (Exception e) {
+            callbackFuture.onError(e);
+        }
+
+        assertTrue(currentUser.isSessionLocked());
+
+        currentUser.removeSessionLock(null);
+    }
+
+    @Test
+    @TargetApi(Build.VERSION_CODES.M)
+    public void testLockSessionAlreadyLocked() throws Exception {
+        MASUser currentUser = MASUser.getCurrentUser();
+        MASCallbackFuture<Void> callbackFuture = new MASCallbackFuture<>();
+        currentUser.lockSession(callbackFuture);
+        try {
+            callbackFuture.get();
+        } catch (Exception e) {
+            callbackFuture.onError(e);
+        }
+
+        assertTrue(currentUser.isSessionLocked());
+        currentUser.lockSession(callbackFuture);
+        assertTrue(currentUser.isSessionLocked());
+
+        currentUser.removeSessionLock(null);
+    }
+
+    @Test(expected = SecureLockException.class)
+    @TargetApi(Build.VERSION_CODES.M)
+    public void testLockSessionMissingIdToken() throws Throwable {
+        MASUser currentUser = MASUser.getCurrentUser();
+        TokenManager tokenManager = StorageProvider.getInstance().getTokenManager();
+        try {
+            tokenManager.deleteIdToken();
+        } catch (TokenStoreException e) {
+        }
+
+        MASCallbackFuture<Void> callbackFuture = new MASCallbackFuture<>();
+        currentUser.lockSession(callbackFuture);
+        try {
+            callbackFuture.get();
+        } catch (Exception e) {
+            throw ((MASException) e.getCause()).getRootCause();
+        }
+    }
+
+    @Test(expected = Exception.class)
+    @TargetApi(Build.VERSION_CODES.M)
+    public void testUnlockSessionCallsOnUserAuthenticationRequired() throws Throwable {
+        MASUser currentUser = MASUser.getCurrentUser();
+        MASSessionUnlockCallbackFuture<Void> callbackFuture = new MASSessionUnlockCallbackFuture<>();
+        currentUser.lockSession(null);
+
+        try {
+            currentUser.unlockSession(callbackFuture);
+            try {
+                callbackFuture.get();
+            } catch (Exception e) {
+                assertEquals(e.getMessage(), "java.lang.Exception: User authentication required.");
+                throw e;
+            }
+        } catch (Exception e) {
+            throw e.getCause();
+        } finally {
+            currentUser.removeSessionLock(null);
+        }
+    }
+
+    @Test
+    @TargetApi(Build.VERSION_CODES.M)
+    public void testUnlockSessionMissingIdToken() throws Throwable {
+        MASUser currentUser = MASUser.getCurrentUser();
+        MASSessionUnlockCallbackFuture<Void> callbackFuture = new MASSessionUnlockCallbackFuture<>();
+        currentUser.lockSession(callbackFuture);
+        currentUser.removeSessionLock(null);
+        currentUser.unlockSession(callbackFuture);
+
+        assertFalse(currentUser.isSessionLocked());
+    }
+
+    @Test
+    @TargetApi(Build.VERSION_CODES.M)
+    public void testRemoveSessionLockAlreadyUnlocked() throws Throwable {
+        MASUser currentUser = MASUser.getCurrentUser();
+        MASSessionUnlockCallbackFuture<Void> callbackFuture = new MASSessionUnlockCallbackFuture<>();
+        currentUser.lockSession(callbackFuture);
+        currentUser.removeSessionLock(null);
+        currentUser.removeSessionLock(callbackFuture);
+
+        assertNull(callbackFuture.get());
+        assertFalse(currentUser.isSessionLocked());
+    }
+}

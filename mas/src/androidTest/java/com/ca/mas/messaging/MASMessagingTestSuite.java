959e8d1872fe54c8833e7d73b634cccbc195f420
Kung, Fergus <Fergus.Kung@ca.com>
2017-04-26 16:21:16 -0700

    [US332907] Moving messaging tests to their own folder

---
+++ mas/src/androidTest/java/com/ca/mas/messaging/MASMessagingTestSuite.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2016 CA. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ *
+ */
+
+package com.ca.mas.messaging;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Categories.class)
+@Suite.SuiteClasses({
+        MASMessagingTest.class,
+})
+
+public class MASMessagingTestSuite {
+}

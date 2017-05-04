f18db10e31b9cd4ac89a514c452d22b191d058c2
Andy <witan03@ca.com>
2017-05-04 08:15:49 -0700

    [US332907] Support Windows file path naming.

--- mas/src/androidTest/java/com/ca/mas/TestUtils.java
+++ mas/src/androidTest/java/com/ca/mas/TestUtils.java
@@ -12,6 +12,8 @@ public class TestUtils {
     private static final int DEFAULT_MAX = 10485760;
 
     public static JSONObject getJSONObject(String path) throws IOException, JSONException {
+        //Support window machine file path.
+        path = path.replace(":", ".");
         path = path + ".json";
         byte[] bytes = IoUtils.slurpStream(TestUtils.class.getResourceAsStream(path), DEFAULT_MAX);
         return new JSONObject(new String(bytes));

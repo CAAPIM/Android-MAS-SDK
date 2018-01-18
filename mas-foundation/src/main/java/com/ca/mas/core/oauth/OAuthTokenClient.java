13c0d13e58f1cb6b34d7f67f10fe56bb0999235d
Andy <witan03@ca.com>
2018-01-18 14:45:18 -0800

    [US436089] - Exception refactoring

--- mas-foundation/src/main/java/com/ca/mas/core/oauth/OAuthTokenClient.java
+++ mas-foundation/src/main/java/com/ca/mas/core/oauth/OAuthTokenClient.java
@@ -12,15 +12,12 @@ import android.support.annotation.NonNull;
 import android.util.Pair;
 
 import com.ca.mas.core.MobileSsoConfig;
-import com.ca.mas.core.auth.AuthenticationException;
 import com.ca.mas.core.client.ServerClient;
 import com.ca.mas.core.context.MssoContext;
 import com.ca.mas.foundation.MASAuthCredentials;
 import com.ca.mas.core.error.MAGErrorCode;
 import com.ca.mas.core.error.MAGException;
 import com.ca.mas.core.error.MAGServerException;
-import com.ca.mas.core.policy.exceptions.InvalidClientCredentialException;
-import com.ca.mas.core.policy.exceptions.RetryRequestException;
 import com.ca.mas.core.request.MAGInternalRequest;
 import com.ca.mas.core.token.IdToken;
 import com.ca.mas.foundation.MASRequest;
@@ -41,8 +38,6 @@ import java.util.List;
  */
 public class OAuthTokenClient extends ServerClient {
 
-    private static final int INVALID_CLIENT_CREDENTIALS = 3003201;
-
     public OAuthTokenClient(MssoContext mssoContext) {
         super(mssoContext);
     }
@@ -57,11 +52,10 @@ public class OAuthTokenClient extends ServerClient {
      * @return a successful JSON response guaranteed to contain a non-empty accessToken and that may additionally contain a new ID token.  Never null.
      * @throws OAuthServerException    if there is an error response from the token server
      * @throws OAuthException          if there is an error other than a valid error JSON response from the token server
-     * @throws AuthenticationException Authentication failed with provider Resource Owner credential
      */
     public OAuthTokenResponse obtainTokensUsingCredentials(@NonNull MAGInternalRequest request,
                                                            @NonNull String clientId,
-                                                           @NonNull String clientSecret, boolean requestIdToken) throws OAuthException, OAuthServerException, AuthenticationException {
+                                                           @NonNull String clientSecret, boolean requestIdToken) throws OAuthException, OAuthServerException {
 
         MASAuthCredentials credentials = request.getGrantProvider().getCredentials(mssoContext);
         if (credentials == null)
@@ -93,16 +87,16 @@ public class OAuthTokenClient extends ServerClient {
                 .build();
 
 
-        OAuthTokenResponse tokenResponse;
+        OAuthTokenResponse tokenResponse = null;
         try {
             tokenResponse = new OAuthTokenResponse(obtainServerResponseToPostedForm(tokenRequest));
+            validate(tokenResponse);
+
         } catch (JSONException | MAGException e) {
             throw new OAuthException(MAGErrorCode.ACCESS_TOKEN_INVALID, e);
         } catch (MAGServerException e) {
             throw new OAuthServerException(e);
         }
-
-        validate(tokenResponse);
 
         return tokenResponse;
     }
@@ -110,18 +104,17 @@ public class OAuthTokenClient extends ServerClient {
     /**
      * Exchange an ID token for an access token that can be used to access APIs.
      *
-     * @param idToken       an ID token, presumably a JWT.  Required.
-     * @param clientId      the client ID of the app seeking access. Required.
-     * @param clientSecret  the client secret of the app seeking access. Required.
-     * @param scope         Scope of access to request.  If null, defaults to "openid msso".
+     * @param idToken      an ID token, presumably a JWT.  Required.
+     * @param clientId     the client ID of the app seeking access. Required.
+     * @param clientSecret the client secret of the app seeking access. Required.
+     * @param scope        Scope of access to request.  If null, defaults to "openid msso".
      * @return a successful JSON response guaranteed to contain a non-empty accessToken.  Never null.
      * @throws OAuthServerException    if there is an error response from the token server
      * @throws OAuthException          if there is an error other than a valid error JSON response from the token server
-     * @throws AuthenticationException Authentication failed with provider Resource Owner credential
      */
     public OAuthTokenResponse obtainAccessTokenUsingIdToken(@NonNull IdToken idToken,
                                                             @NonNull String clientId,
-                                                            @NonNull String clientSecret, String scope) throws OAuthException, AuthenticationException, OAuthServerException, RetryRequestException {
+                                                            @NonNull String clientSecret, String scope) throws OAuthException, OAuthServerException {
         if (scope == null)
             scope = OPENID_PHONE_EMAIL;
 
@@ -139,23 +132,18 @@ public class OAuthTokenClient extends ServerClient {
                 .build();
 
 
-        OAuthTokenResponse tokenResponse;
+        OAuthTokenResponse tokenResponse = null;
         try {
             tokenResponse = new OAuthTokenResponse(obtainServerResponseToPostedForm(tokenRequest));
+            validate(tokenResponse);
         } catch (JSONException | MAGException e) {
             throw new OAuthException(MAGErrorCode.ACCESS_TOKEN_INVALID, e);
         } catch (MAGServerException e) {
-            //Throw error to retry request instead of ask for credential
-            if (e.getErrorCode() == INVALID_CLIENT_CREDENTIALS) {
-                throw new InvalidClientCredentialException(e);
-            }
             throw new OAuthServerException(e);
         }
-
-        validate(tokenResponse);
-
         return tokenResponse;
-    }
+
+   }
 
     private void validate(OAuthTokenResponse tokenResponse) throws OAuthException {
         if (!tokenResponse.isBearer())
@@ -168,17 +156,16 @@ public class OAuthTokenClient extends ServerClient {
     /**
      * Exchange a refresh token for a new access token that can be used to access APIs.
      *
-     * @param refreshToken  a refresh token.  Required.
-     * @param clientId      the client ID of the app seeking access. Required.
-     * @param clientSecret  the client secret of the app seeking access. Required.
+     * @param refreshToken a refresh token.  Required.
+     * @param clientId     the client ID of the app seeking access. Required.
+     * @param clientSecret the client secret of the app seeking access. Required.
      * @return a successful JSON response guaranteed to contain a non-empty accessToken.  Never null.
      * @throws OAuthServerException    if there is an error JSON response from the token server
      * @throws OAuthException          if there is an error other than a valid error JSON response from the token server
-     * @throws AuthenticationException Authentication failed with provider Resource Owner credential
      */
     public OAuthTokenResponse obtainTokenUsingRefreshToken(@NonNull String refreshToken,
                                                            @NonNull String clientId,
-                                                           @NonNull String clientSecret) throws OAuthException, AuthenticationException, OAuthServerException, RetryRequestException {
+                                                           @NonNull String clientSecret) throws OAuthException, OAuthServerException {
 
         List<Pair<String, String>> form = new ArrayList<>();
         form.add(new Pair<String, String>(REFRESH_TOKEN, refreshToken));
@@ -192,26 +179,22 @@ public class OAuthTokenClient extends ServerClient {
                 .responseBody(MASResponseBody.stringBody())
                 .build();
 
-        OAuthTokenResponse tokenResponse;
+        OAuthTokenResponse tokenResponse = null;
 
         //Remove refresh token once we get a proper response from the server, no matter success or not.
         mssoContext.takeRefreshToken();
 
         try {
             tokenResponse = new OAuthTokenResponse(obtainServerResponseToPostedForm(tokenRequest));
+            validate(tokenResponse);
         } catch (JSONException | MAGException e) {
             throw new OAuthException(MAGErrorCode.ACCESS_TOKEN_INVALID, e);
         } catch (MAGServerException e) {
-            //Throw error to retry request instead of using the id_token
-            if (e.getErrorCode() == INVALID_CLIENT_CREDENTIALS) {
-                throw new InvalidClientCredentialException(e);
-            }
             throw new OAuthServerException(e);
         }
-
-        validate(tokenResponse);
-
         return tokenResponse;
     }
+
+
 
 }

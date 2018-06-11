11707441db6aaf5abeb6f642ae14d15d0774c68d
Isabel Martin <marma63@ca.com>
2018-06-11 16:52:18 -0700

    - extend MASSharedStorage with encryptation + Unit Test

---
+++ mas-foundation/src/androidTest/java/com/ca/mas/foundation/SecureSharedStorageTest.java
@@ -0,0 +1,289 @@
+package com.ca.mas.foundation;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+
+import com.ca.mas.MASLoginTestBase;
+import com.ca.mas.core.storage.securesharedstorage.SecureSharedStorage;
+import com.ca.mas.core.storage.sharedstorage.MASSharedStorage;
+
+import org.junit.After;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
+
+public class SecureSharedStorageTest extends MASLoginTestBase {
+
+    private final String accountName = "testNameEncrypted";
+    private final String accountNameb = "testNameNoEncripted";
+
+    private final String accountType = "com.mas.foundation.sharedstorage";
+
+    @Test
+    public void testStorageCreation() {
+        SecureSharedStorage storage = new SecureSharedStorage(accountName, true);
+
+        AccountManager am = AccountManager.get(getContext());
+        Account[] accounts = am.getAccountsByType(accountType);
+        assertEquals(0, accounts.length);
+    }
+
+    @Test
+    public void testStorageCreationMissingParameters() {
+        try {
+            SecureSharedStorage storage = new SecureSharedStorage(null, true);
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+        try {
+            SecureSharedStorage storage = new SecureSharedStorage("", true);
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+    }
+
+    @Test
+    public void testStorageSaveGetString() throws Exception {
+        String key = "testKey123";
+        String object = "testValue123";
+        SecureSharedStorage storage = new SecureSharedStorage(accountName, true);
+        storage.save(key, object);
+
+        SecureSharedStorage storageb = new SecureSharedStorage(accountNameb, false);
+        storageb.save(key,object);
+
+        String retrievedObject = storage.getString(key);
+        String retrievedObjectb = storageb.getString(key);
+
+        assertEquals(retrievedObject, retrievedObjectb);
+    }
+
+    @Test
+    public void testStorageSaveGetBytes() throws Exception {
+        String key = "testKey123";
+        byte[] bytes = "testValue12345".getBytes();
+
+        SecureSharedStorage storage = new SecureSharedStorage(accountName, true);
+        storage.save(key, bytes);
+
+        SecureSharedStorage storageb = new SecureSharedStorage(accountNameb, false);
+        storageb.save(key, bytes);
+
+
+        byte[] retrievedObject = storage.getBytes(key);
+        byte[] retrievedObjectb = storageb.getBytes(key);
+
+        assertTrue(Arrays.equals(retrievedObject, retrievedObjectb));
+    }
+
+    @Test
+    public void testStorageDeleteGetString() throws Exception {
+        String key = "testKey123";
+        String value = "testValue12345";
+
+        SecureSharedStorage storage = new SecureSharedStorage(accountName, true);
+        storage.save(key, value);
+        storage.delete(key);
+
+        SecureSharedStorage storageb = new SecureSharedStorage(accountNameb, true);
+        storageb.save(key, value);
+        storageb.delete(key);
+
+        String retrievedString = storage.getString(key);
+        assertNull(retrievedString);
+
+        String retriveStringb = storageb.getString(key);
+        assertNull(retriveStringb);
+
+        byte[] retrievedData = storage.getBytes(key);
+        assertNull(retrievedData);
+
+        byte[] retrievedDatab = storageb.getBytes(key);
+        assertNull(retrievedDatab);
+    }
+
+
+    @Test(expected = NullPointerException.class)
+    public void testStorageErrorSaveNullKey() {
+        SecureSharedStorage storage = new SecureSharedStorage(accountName, true);
+        String data = "testValue12345";
+        byte[] bytes = "testValue12345".getBytes();
+
+        try {
+            storage.save(null, data);
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+        try {
+            storage.save(null, bytes);
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+        try {
+            storage.save("", data);
+            fail();
+        } catch (NullPointerException e) {
+        }
+
+        storage.save("", bytes);
+        fail();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testStorageErrorSaveNullKeyFalseMode() {
+        SecureSharedStorage storage = new SecureSharedStorage(accountName, false);
+        String data = "testValue12345";
+        byte[] bytes = "testValue12345".getBytes();
+
+        try {
+            storage.save(null, data);
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+        try {
+            storage.save(null, bytes);
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+        try {
+            storage.save("", data);
+            fail();
+        } catch (NullPointerException e) {
+        }
+
+        storage.save("", bytes);
+        fail();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testStorageErrorDeleteNullKey() {
+        SecureSharedStorage storage = new SecureSharedStorage(accountName,true);
+
+        try {
+            storage.delete(null);
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+        storage.delete("");
+        fail();
+
+        SecureSharedStorage storageb = new SecureSharedStorage(accountName,false);
+
+        try {
+            storageb.delete(null);
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+        storageb.delete("");
+        fail();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testStorageErrorRetrieveNullKey() {
+        SecureSharedStorage storage = new SecureSharedStorage(accountName,true);
+
+        try {
+            storage.getString(null);
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+        try {
+            storage.getBytes(null);
+            fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+        try {
+            storage.getString("");
+            fail();
+        } catch (NullPointerException e) {
+        }
+
+        storage.getBytes("");
+        fail();
+    }
+
+    @Test
+    public void saveKeyEncriptedRetrieveUnEncripted() {
+        String key = "testKey123";
+        String value = "testValue12345";
+        String comparea = "";
+        String compareb = "";
+
+        SecureSharedStorage storage = new SecureSharedStorage(accountName, true);
+        storage.save(key, value);
+        comparea = storage.getString(key);
+
+        SecureSharedStorage storageb = new SecureSharedStorage(accountName, false);
+        compareb = storageb.getString(key);
+
+        if (!comparea.equals(compareb)) {
+            assert(true);
+        }
+        else {
+            assert(false);
+        }
+    }
+
+
+    @Test(expected = RuntimeException.class)
+    public void saveKeyUnencriptedRetrieveEncripted() {
+        String key = "testKey123";
+        String value = "testValue12345";
+        String comparea = "";
+        String compareb = "";
+
+        SecureSharedStorage storage = new SecureSharedStorage(accountName, false);
+        storage.save(key, value);
+        comparea = storage.getString(key);
+
+        SecureSharedStorage storageb = new SecureSharedStorage(accountName, true);
+        compareb = storageb.getString(key);
+    }
+
+    @Test
+    public void SecureVSSharedStorageNoConflic() {
+
+        String key = "testKey123";
+        String value = "testValue12345";
+        String comparea = "";
+        String compareb = "";
+
+        MASSharedStorage storage = new MASSharedStorage(accountName);
+        storage.save(key, value);
+        comparea = storage.getString(key);
+
+        SecureSharedStorage storageb = new SecureSharedStorage(accountNameb, true);
+        storageb.save(key,value);
+        compareb = storageb.getString(key);
+
+        assertEquals(comparea, compareb);
+    }
+
+
+    @After
+    public void resetAccountsAndData() {
+        AccountManager am = AccountManager.get(getContext());
+        Account[] accounts = am.getAccountsByType(accountType);
+        for (Account account : accounts) {
+            try {
+                am.removeAccountExplicitly(account);
+            } catch (NoSuchMethodError e) {
+                //ignore
+            }
+        }
+    }
+}
--- mas-foundation/src/main/java/com/ca/mas/core/storage/securesharedstorage/SecureSharedStorage.java
+++ mas-foundation/src/main/java/com/ca/mas/core/storage/securesharedstorage/SecureSharedStorage.java
@@ -7,36 +7,40 @@ import com.ca.mas.core.security.EncryptionProvider;
 import com.ca.mas.core.storage.sharedstorage.MASSharedStorage;
 import com.ca.mas.foundation.MAS;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 
 public class SecureSharedStorage extends MASSharedStorage {
 
-    private EncryptionProvider encProvider;
-    private boolean secureMode = false;
+    private EncryptionProvider encProvider = null;
 
     /**
-     * Creates or retrieves a MASSharedStorage with the specified name and account type.
-     * Ensure that this does not conflict with any existing accountType on the device.
+     * Creates or retrieves a SecureSharedStorage with the specified name.
+     * Ensure that this does not conflict with any existing accountName on the device.
      *
      * @param accountName the name of the account to be created in the AccountManager
      */
     public SecureSharedStorage(@NonNull String accountName, boolean activeSecureMode) {
         super(accountName);
 
-        if (secureMode) {
+        if (activeSecureMode) {
             encProvider = new DefaultEncryptionProvider(MAS.getContext());
         }
     }
 
     @Override
-    public void save(@NonNull String key, String value) {
+    public void save(@NonNull String key, String value){
         isValidKey(key);
 
         String retValue = value;
 
         if (encProvider != null) {
-            byte[] encryptedValue = encProvider.encrypt(value.getBytes());
-            retValue = new String(encryptedValue, Charset.forName("UTF-8"));
+            try {
+                byte[] encryptedValue = encProvider.encrypt(value.getBytes());
+                retValue = new String(encryptedValue, "ISO-8859-1");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
         }
 
         super.save(key, retValue);
@@ -63,13 +67,17 @@ public class SecureSharedStorage extends MASSharedStorage {
 
     @Override
     public String getString(String key) {
-        //TODO: what happend if I try to get some data that was stored without encryptation, and now is active the encryptation mode ??
         isValidKey(key);
 
         String retValue = super.getString(key);
 
-        if (encProvider != null) {
-            retValue = new String(encProvider.decrypt(retValue.getBytes()), Charset.forName("UTF-8"));
+        if (encProvider != null && retValue != null) {
+            try {
+                byte[] encodedbytes = encProvider.decrypt(retValue.getBytes("ISO-8859-1"));
+                retValue = new String(encodedbytes, Charset.forName("UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
         }
 
         return retValue;
@@ -77,9 +85,13 @@ public class SecureSharedStorage extends MASSharedStorage {
 
     @Override
     public byte[] getBytes(String key) {
-        //TODO: what happend if I try to get some data that was stored without encryptation, and now is active the encryptation mode ??
         isValidKey(key);
-        return super.getBytes(key);
+
+        byte[] retValue = super.getBytes(key);
+        if (encProvider != null && retValue != null) {
+            retValue = encProvider.decrypt(retValue);
+        }
+        return retValue;
     }
 
     private void isValidKey(String key) {

afdf7f9e9259c6b8f2f9b5cb83998a04b8db88f9
am654971 <arindam.mukherjee@broadcom.com>
2021-06-14 20:05:55 +0530

    SSL Pinning changes

--- mas-foundation/src/main/java/com/ca/mas/foundation/MASSecurityConfiguration.java
+++ mas-foundation/src/main/java/com/ca/mas/foundation/MASSecurityConfiguration.java
@@ -23,12 +23,14 @@ public interface MASSecurityConfiguration {
     Uri getHost();
     boolean isPublic();
     boolean trustPublicPki();
+    boolean isSslPinningEnabled();
     List<Certificate> getCertificates();
     List<String> getPublicKeyHashes();
 
     class Builder {
 
         private boolean isPublic;
+        private boolean isSSLPinningEnabled = false;
         private boolean trustPublicPKI;
 
         private List<Certificate> certificates;
@@ -53,6 +55,16 @@ public interface MASSecurityConfiguration {
          */
         public Builder trustPublicPKI(boolean trust) {
             this.trustPublicPKI = trust;
+            return this;
+        }
+
+        /**
+         * Determines whether or not to enable ssl pinning to primary gateway.
+         * @param sslPinning to include or not
+         * @return the builder object
+         */
+        public Builder enableSSLPinning(boolean sslPinning) {
+            this.isSSLPinningEnabled = sslPinning;
             return this;
         }
 
@@ -104,7 +116,7 @@ public interface MASSecurityConfiguration {
             }
 
             // If trustPublicPKI is false and no pinning information is found, throw an exception.
-            if (!trustPublicPKI && publicKeyHashes == null && certificates == null) {
+            if (isSSLPinningEnabled && !trustPublicPKI && publicKeyHashes == null && certificates == null) {
                 throw new IllegalArgumentException("Missing pinning type, cannot establish SSL.");
             }
 
@@ -132,6 +144,11 @@ public interface MASSecurityConfiguration {
                 @Override
                 public boolean trustPublicPki() {
                     return trustPublicPKI;
+                }
+
+                @Override
+                public boolean isSslPinningEnabled() {
+                    return isSSLPinningEnabled;
                 }
             };
         }
